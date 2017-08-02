12c4953f224188591c954f7c4e82490df900f5ec
XBlueSky <30610447+XBlueSky@users.noreply.github.com>
2017-08-02 10:30:41 +0800

    import sharply.h

--- sharply.cpp
+++ sharply.cpp
@@ -1,29 +1,9 @@
-#include <iostream>
-#include <math.h>
-#include <vector>
-#include <utility>
-#include <cstring>
-#include <string>  
-#include <sstream>
-using namespace std;
+#include "sharply.h"
 
 string subset[1001];
 vector<pair<int,string> > player_v;
 vector<pair<int,string> > player_sharply;
 vector<string> part;
-
-int factorial(int n){
-	int temp = 1;
-	if(n > 0){
-		for(int i = 1; i <= n; i++)
-			temp *= i;	
-	}
-	return temp;
-}
-
-int c(int n,int m){
-	return	factorial(n)/(factorial(n-m)*factorial(m));	
-}
 
 void combination(int now,int start,int n,int m){
 	string temp ="";
@@ -41,17 +21,6 @@ void combination(int now,int start,int n,int m){
 			subset[now]=str;
     		combination(now+1,j+1,n,m);
    }
-}
-
-int find_P(string str){
-	int count = 0, start = 0;
-	while(str.find("P",start) != -1){
-		if(str.find("P",start) != -1){
-			count++;
-			start = str.find("P",start+1);
-		}	
-	}
-	return count;
 }
 
 int main(){
